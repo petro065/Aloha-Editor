0aa262e26407bb25607cb3c50af7b28159735059
Petro Salema <p.salema@gentics.com>
2011-12-27 11:52:46 +0100

    Refactoring

--- src/plugins/extra/thunderbird/lib/thunderbird-plugin.js
+++ src/plugins/extra/thunderbird/lib/thunderbird-plugin.js
@@ -1,42 +1,41 @@
 /*!
-* Aloha-Editor adapter for Thunderbird
-* ------------------------------------
-* Will dynamically load aloha stylesheets which cannot be included inline in
-* the html document.
-*/
+ * Aloha-Editor adapter for Thunderbird
+ * ------------------------------------
+ * Will dynamically load aloha stylesheets which cannot be included inline in
+ * the html document.
+ */
 
 define( [
 	'aloha',
 	'aloha/jquery',
 	'aloha/floatingmenu',
 	'css!thunderbird/css/aloha.css'
-], function ( Aloha, jQuery, FloatingMenu ) {
+], function( Aloha, jQuery, FloatingMenu ) {
 	'use strict'
-	
-	jQuery( function () {
+
+	jQuery( function() {
 		jQuery( '.alohafy' ).aloha();
 	} );
-	
+
 	// Prevents the floating menu from floating out of view
-	Aloha.bind( 'aloha-selection-changed', function () {
-		setTimeout( function () {
-			var fm = FloatingMenu,
-			    pos = fm.obj.offset(),
+	Aloha.bind( 'aloha-selection-changed', function() {
+		setTimeout( function() {
+			var pos = FloatingMenu.obj.offset(),
 			    left = pos.left,
 			    top = pos.top,
 			    reposition = false;
-			
+
 			if ( top < 0 ) {
 				top = 0;
 				reposition = true;
 			}
-			
+
 			if ( left < 0 ) {
 				left = 0;
 				reposition = true;
 			}
-			
+
 			FloatingMenu.floatTo( { top: top, left: left } );
 		}, 100 );
-	} );	
+	} );
 } );
