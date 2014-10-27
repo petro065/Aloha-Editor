43dc382aecb9a6a862cf767bf3836e7715b50f07
Petro Salema <p.salema@gentics.com>
2014-10-27 21:04:23 +0100

    extractCollapsed re-implemented

--- src/deranged.js
+++ src/deranged.js
@@ -1,5 +1,5 @@
 /**
- * verticals.js is part of Aloha Editor project http://aloha-editor.org
+ * deranged.js is part of Aloha Editor project http://aloha-editor.org
  *
  * Aloha Editor is a WYSIWYG HTML5 inline editing library and editor.
  * Copyright (c) 2010-2014 Gentics Software GmbH, Vienna, Austria.
@@ -39,92 +39,7 @@ define([
 
 	var TESTS = true;
 
-	function closest(node, match) {
-		return Dom.upWhile(node, Fn.complement(match));
-	}
-
-	var VOID_MARKER = '♞';
-	var LEAF = 1 << 0;
-	var TEXT = 1 << 1;
-	var VOID = 1 << 2;
-	var META = 1 << 3;
-
-	function getBit(node) {
-		if (Dom.isTextNode(node)) {
-			return TEXT | LEAF;
-		}
-		if (Html.isVoidNode(node)) {
-			return VOID | LEAF;
-		}
-		return META;
-	}
-
-	function extractFormatting(element, offset) {
-		offset = offset || 0;
-		var ranges = [];
-		var snippets = [];
-		var wasText = false;
-		Dom.children(element).forEach(function (node) {
-			var bit = getBit(node);
-			var length;
-			if (bit & TEXT) {
-				if (wasText) {
-					ranges.push([offset, offset, 'split']);
-				}
-				wasText = true;
-				snippets.push(node.data);
-				length = node.data.length;
-			} else if (bit & VOID) {
-				wasText = false;
-				snippets.push(VOID_MARKER);
-				length = 1;
-			} else {
-				wasText = false;
-				var more = extractFormatting(node, offset);
-				ranges = ranges.concat(
-					[[offset, offset + more.content.length, node]],
-					more.ranges
-				);
-				snippets.push(more.content);
-				length += more.content.length;
-			}
-			offset += length;
-		});
-		return {
-			content   : snippets.join(''),
-			collapsed : [],
-			ranges    : ranges
-		};
-	}
-
-	var zwChars = Strings.ZERO_WIDTH_CHARACTERS.join('');
-	var breakingWhiteSpaces = Arrays.difference(
-		Strings.WHITE_SPACE_CHARACTERS,
-		Strings.NON_BREAKING_SPACE_CHARACTERS
-	).join('');
-
-	var NOT_WSP_FROM_START = new RegExp('[^' + breakingWhiteSpaces + zwChars + ']');
-	var WSP_FROM_START = new RegExp('[' + breakingWhiteSpaces + ']');
-
-	function splice(yarn, start, count, text) {
-		var end = start + count;
-		var insert = text || '';
-		var content = yarn.content.substring(0, start)
-		            + insert
-		            + yarn.content.substring(end);
-
-		var adjusted = yarn.ranges.reduce(function (ranges, range) {
-			var a = range[0];
-			var b = range[1];
-			return ranges.concat(range);
-		}, []);
-
-		return {
-			content   : content,
-			ranges    : adjusted.concat(),
-			collapsed : yarn.collapsed.concat()
-		};
-	}
+	// `readMarkers
 
 	function readMarkers(marked) {
 		var onlyBrackets = marked.replace(/[\{\}\|]/g, '');
@@ -185,303 +100,260 @@ define([
 		});
 	}
 
-	function remove(content, start, end, preserve) {
-		var mutated = content.substr(0, start) + content.substr(end);
-		var a = preserve[0];
-		var b = preserve[1];
-		if (!a || !b || (a >= start && b <= end)) {
-			return {
-				content   : mutated,
-				preserved : []
-			};
-		}
-		if (start < a) {
-			a = a - (Math.min(a, end) - start);
-		}
-		if (start < b) {
-			b = b - (Math.min(b, end) - start);
-		}
+	// `remove
+
+	function remove(deranged, start, end) {
+		var content = deranged.content;
 		return {
-			content   : mutated,
-			preserved : [a, b]
+			content : content.substring(0, start) + content.substring(end),
+			ranges  : deranged.ranges.reduce(function (list, item) {
+				var a = item[0];
+				var b = item[1];
+				if (!a || !b || (a >= start && b <= end)) {
+					return list.concat([[]]);
+				}
+				if (start < a) {
+					a = a - (Math.min(a, end) - start);
+				}
+				if (start < b) {
+					b = b - (Math.min(b, end) - start);
+				}
+				return list.concat([[a, b].concat(item.slice(2))]);
+			}, [])
 		};
 	}
 
 	if (TESTS) {
-		[
+		(function () {
+			var original = [];
+			var expected = [];
+			[
 
-			// 0 1 2 3 4 5 6 7 8 9
-			['012[345]6789',   '0126789'  ],
+				//   |
+				//   { }
+				//   {   }
+				//   {       }
+				//   {             }
+				//       [     ]
+				// 0 1 2 3 4 5 6 7 8 9
+				['0{1}23456789', '0{1}26789'],
+				['0{12}3456789', '0{12}6789'],
+				['0{1234}56789', '0{12}6789'],
+				['0{1234567}89', '0{1267}89'],
 
-			//   |
-			// 0 1 2 3 4 5 6 7 8 9
-			['0|12[345]6789',  '0|126789' ],
+				//       |
+				//       {   }
+				//       {     }
+				//       {         }
+				//       [     ]
+				// 0 1 2 3 4 5 6 7 8 9
+				['012{34}56789', '0126789'],
+				['012{345}6789', '0126789'],
+				['012{34567}89', '012{67}89'],
 
-			//   { }
-			//   {   }
-			//   {       }
-			//   {             }
-			//       [     ]
-			// 0 1 2 3 4 5 6 7 8 9
-			['0{1}2[345]6789', '0{1}26789'],
-			['0{12}[345]6789', '0{12}6789'],
-			['0{12[34}5]6789', '0{12}6789'],
-			['0{12[345]67}89', '0{1267}89'],
+				//         |
+				//         { }
+				//         {   }
+				//         {       }
+				//       [     ]
+				// 0 1 2 3 4 5 6 7 8 9
+				['0123{4}56789', '0126789'],
+				['0123{45}6789', '0126789'],
+				['0123{4567}89', '012{67}89'],
 
-			//       |
-			//       {   }
-			//       {     }
-			//       {         }
-			//       [     ]
-			// 0 1 2 3 4 5 6 7 8 9
-			['012{[34}5]6789', '0126789'],
-			['012{[345}]6789', '0126789'],
-			['012[{345]67}89', '012{67}89'],
+				//             |
+				//             {   }
+				//       [     ]
+				// 0 1 2 3 4 5 6 7 8 9
+				['012345{67}89', '012{67}89'],
 
-			//         |
-			//         { }
-			//         {   }
-			//         {       }
-			//       [     ]
-			// 0 1 2 3 4 5 6 7 8 9
-			['012[3{4}5]6789', '0126789'],
-			['012[3{45}]6789', '0126789'],
-			['012[3{45]67}89', '012{67}89'],
+				//               |
+				//               { }
+				//       [     ]
+				// 0 1 2 3 4 5 6 7 8 9
+				['0123456{7}89', '0126{7}89']
+			].forEach(function (test) {
+				original.push(readMarkers(test[0]).braces);
+				expected.push(readMarkers(test[1]).braces);
+			});
+			var range = readMarkers('012{345}6789').braces;
+			var result = remove({
+				content : '0123456789',
+				ranges  : original
+			}, range[0], range[1]);
+			if ('0126789' !== result.content) {
+				console.error('remove() tests: result.content don\'t match');
+			}
+			var preserved = result.ranges;
+			expected.forEach(function (expected, index) {
+				if (expected.toString() !== preserved[index].toString()) {
+					console.error(
+						'remove() tests #' + index + ': Expected "',
+						expected.toString() || '   ',
+						'" but got "',
+						preserved[index].toString() || '   ',
+						'"'
+					);
+				}
+			});
+		}());
+	}
 
-			//             |
-			//             {   }
-			//       [     ]
-			// 0 1 2 3 4 5 6 7 8 9
-			['012[345{]67}89', '012{67}89'],
+	var VOID_MARKER = '♞';
+	var LEAF = 1 << 0;
+	var TEXT = 1 << 1;
+	var VOID = 1 << 2;
+	var META = 1 << 3;
 
-			//               |
-			//               { }
-			//       [     ]
-			// 0 1 2 3 4 5 6 7 8 9
-			['012[345]6{7}89', '0126{7}89']
+	function getBit(node) {
+		if (Dom.isTextNode(node)) {
+			return TEXT | LEAF;
+		}
+		if (Html.isVoidNode(node)) {
+			return VOID | LEAF;
+		}
+		return META;
+	}
 
-		].forEach(function (test) {
-			var original = readMarkers(test[0]);
-			var expected = readMarkers(test[1]);
-			var result = remove(
-				original.content,
-				original.brackets[0],
-				original.brackets[1],
-				original.braces
-			);
-			if (result.preserved.toString() !== expected.braces.toString()) {
+	function parseDom(element, offset) {
+		offset = offset || 0;
+		var ranges = [];
+		var snippets = [];
+		var wasText = false;
+		Dom.children(element).forEach(function (node) {
+			var bit = getBit(node);
+			var length;
+			if (bit & TEXT) {
+				if (wasText) {
+					ranges.push([offset, offset, 'split']);
+				}
+				wasText = true;
+				snippets.push(node.data);
+				length = node.data.length;
+			} else if (bit & VOID) {
+				wasText = false;
+				snippets.push(VOID_MARKER);
+				length = 1;
+			} else {
+				wasText = false;
+				var more = parseDom(node, offset);
+				ranges = ranges.concat(
+					[[offset, offset + more.content.length, node]],
+					more.ranges
+				);
+				snippets.push(more.content);
+				length += more.content.length;
+			}
+			offset += length;
+		});
+		return {
+			content   : snippets.join(''),
+			collapsed : [],
+			ranges    : ranges
+		};
+	}
+
+	// `parseDom
+
+	if (TESTS) {
+		[{
+			markup  : '<div>foo<u>ba<i>r</i> baz</u><img/>  qux</div>',
+			content : 'foobar baz' + VOID_MARKER + '  qux',
+			control : 'foo{ba[r] baz}' + VOID_MARKER + '  qux',
+			markers : {
+				U : 'braces',
+				I : 'brackets'
+			}
+		}].forEach(function (test) {
+			var deranged = parseDom(Html.parse(test.markup, document)[0]);
+			if (test.content !== deranged.content) {
 				console.error(
-					'remove() test:',
-					'Expected "' + expected.braces.toString()
-						+ '", but got "' + result.preserved.toString() + '"'
+					'parseDom() test:',
+					'Expected "', test.content, '" ',
+					'but got "', deranged.content, '"'
 				);
 			}
+			var markers = readMarkers(test.control);
+			deranged.ranges.forEach(function (range) {
+				var marker = test.markers[range[2].nodeName];
+				if (marker && (range.slice(0, -1).toString() !==  markers[marker].toString())) {
+					console.error(
+						'parseDom() test:',
+						'Expected "', markers[marker].toString(), '" ',
+						'but got "', range.slice(0, -1).toString(), '"'
+					);
+				}
+			});
 		});
 	}
 
+	// `extractCollapsed
+	//
+	var zwChars = Strings.ZERO_WIDTH_CHARACTERS.join('');
+	var breakingWhiteSpaces = Arrays.difference(
+		Strings.WHITE_SPACE_CHARACTERS,
+		Strings.NON_BREAKING_SPACE_CHARACTERS
+	).join('');
 
-			/*
+	var NOT_WSP_FROM_START = new RegExp('[^' + breakingWhiteSpaces + zwChars + ']');
+	var WSP_FROM_START = new RegExp('[' + breakingWhiteSpaces + ']');
 
-			// start < a && end < b
-			//   [ ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//       {     }
-			// 0 2 3 4 5 6 7 8 9   mutated
-			//
-			// 1                   removed
-			[1, 2, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start < a && end > start && end < b
-			//   [       ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//   {   }
-			// 0 5 6 7 8 9         mutated
-			//       { }
-			// 1 2 3 4             removed
-			[1, 5, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start < a && end == b
-			//   [           ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//
-			// 0 7 8 9             mutated
-			//       {     }
-			// 1 2 3 4 5 6         removed
-			[1, 7, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start < a && end > b
-			//   [             ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//
-			// 0 8 9               mutated
-			//       {     }
-			// 1 2 3 4 5 6 7       removed
-			[1, 9, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start == a && end < b
-			//         [   ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//         { }
-			// 0 1 2 3 6 7 8 9     mutated
-			// {   }
-			// 4 5                 removed
-			[4, 6, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start == a && end == b
-			//         [     ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//
-			// 0 1 2 3 7 8 9       mutated
-			// {     }
-			// 4 5 6               removed
-			[4, 7, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start == a && end > b
-			//         [       ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//
-			// 0 1 2 3 8 9         mutated
-			// {     }
-			// 4 5 6 7 8           removed
-			[4, 8, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start > a && end < b
-			//           [ ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//         {   }
-			// 0 1 2 3 4 6 7 8 9   mutated
-			// {   }
-			// 5 6                 removed
-			[5, 6, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start > a && end == b
-			//           [   ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//
-			// 0 1 2 3 4 7 8 9     mutated
-			//
-			// 5 6                 removed
-			[5, 7, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start > a && end > b
-			//           [     ]
-			//         {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//         { }
-			// 0 1 2 3 4 8 9       mutated
-			// {     }
-			// 4 5 6 7             removed
-			[5, 8, {start:true}],
-			[4, 7, {end  :true}],
-
-			// start > a && end > b
-			//           [   ]
-			//   {     }
-			// 0 1 2 3 4 5 6 7 8 9 content
-			//   {     }
-			// 0 1 2 3 4 7 8 9     mutated
-			//
-			// 5 6                 removed
-			[5, 7, {start:true}],
-			[1, 4, {end  :true}]
-
-			*/
-
-
-	function extractCollapsed(yarn) {
+	function extractCollapsed(deranged) {
+		var content = deranged.content;
 		var collapsed = [];
 		var offset = 0;
+		var guard = 99;
 		var match;
-		var snippet = yarn.content;
-		while (yarn.content.length > offset) {
-			match = snippet.search(NOT_WSP_FROM_START);
-			// Because `snippet` consists only of white spaces
-			// eg: "   "
+		while (--guard) {
+			match = content.search(NOT_WSP_FROM_START);
+			// Only whitespaces
 			if (-1 === match) {
-				collapsed.push([offset, snippet]);
-				yarn = splice(yarn, offset, snippet.length);
+				collapsed.push([offset, content.substring(0, content)]);
 				break;
-			} else if (0 === match) {
-				// Because `snippet` contains no leading white spaces
-				// eg: "foo bar"
-				match = snippet.search(WSP_FROM_START);
-				// Because there are no more white spaces
+			}
+			// No leading whitespaces
+			if (0 === match) {
+				match = content.search(WSP_FROM_START);
+				// No more white spaces
 				if (-1 === match) {
 					break;
 				}
 				offset += match;
-				snippet = snippet.substring(match);
+				content = content.substring(match);
+
+			// Leading white space found
+			// eg: " foo bar"
+			// But multiple spaces should be replaced with a single space
+			// *except* at the beginning and end of the string
+			} else if (0 === offset || match === content.length) {
+				collapsed.push([offset, content.substring(0, match)]);
+				deranged = remove(deranged, offset, offset + match);
+				content = deranged.content;
+			} else if (1 === match) {
+				offset++;
+				content = content.substring(1);
 			} else {
-				// Because leading white space is found
-				// eg: " foo bar"
-				// But multiple spaces should be replaced with a single space
-				// *except* at the beginning and end of the string
-				if (offset > 0 && match < snippet.length) {
-					offset++;
-					collapsed.push([offset, snippet.substring(1, match)]);
-					yarn = splice(yarn, offset, match - 1);
-				} else {
-					collapsed.push([offset, snippet.substring(0, match)]);
-					yarn = splice(yarn, offset, match);
-				}
-				snippet = snippet.substring(match);
+				offset++;
+				collapsed.push([offset, content.substring(1, match)]);
+				deranged = remove(deranged, offset, offset + match - 1);
+				content = content.substring(match);
 			}
 		}
-		return yarn;
+		deranged.collapsed = collapsed;
+		return deranged;
 	}
 
-	function create(boundaries) {
-		return;
-		var block = closest(
-			Boundaries.container(boundaries[0]),
-			Html.hasLinebreakingStyle
-		);
-		Dom.remove(block.querySelector('br'));
-		console.log(block.innerHTML.replace(/\s/g, '·'));
-
-		var yarn = extractFormatting(block);
-		var result = extractCollapsed(yarn);
-
-		console.log(result.content);
-
-		yarn = result;
-		var offset = 0;
-		var str = yarn.content;
-		var ranges = yarn.ranges;
-		Maps.forEach(ranges, function (range) {
-			var start = range[0] + offset;
-			var end = range[1] + offset;
-			if ('split' === range[2]) {
-				str = str.substring(0, start) + '|' + str.substring(end);
-				offset++;
-			} else {
-				var mark = '[' + range[2].nodeName + ' ';
-				str = str.substring(0, start)
-				    + mark + str.substring(start, end) + ']'
-				    + str.substring(end);
-				offset += mark.length;
-			}
+	if (TESTS) {
+		[{
+			markup  : '<div> &nbsp;foo &nbsp;&nbsp;<u>ba<i>r</i> baz</u>      qux</div>',
+			content : ' foo   bar baz qux',
+		}].forEach(function (test) {
+			var deranged = parseDom(Html.parse(test.markup, document)[0]);
+			var deranged2 = extractCollapsed(deranged);
+			console.log('|' + deranged.content + '|');
+			console.log(deranged);
 		});
-		console.log('{' + str + '}');
 	}
 
-	window.verticals = create;
+	window.verticals = function () {};
 });
