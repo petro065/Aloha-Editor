c8366cd320e65b203d54138f2cefd47ee1800b1b
Petro Salema <p.salema@gentics.com>
2015-01-07 10:55:51 +0100

    Parallax

--- docs/js/aloha-docs.js
+++ docs/js/aloha-docs.js
@@ -1,4 +1,4 @@
-(function (aloha, CodeMirror) {
+(function (aloha, $, CodeMirror) {
 	'use strict';
 
 	aloha.dom.query('.snippet', document).forEach(function (elem) {
@@ -10,9 +10,75 @@
 			readOnly : true
 		});
 		elem.innerHTML = elem.innerHTML
-			.replace(/<span class="cm-operator">&lt;\*<\/span>/g, '<u>')
-			.replace(/<span class="cm-operator">\*&gt;<\/span>/g, '</u>')
-			.replace(/{%/g, '<u>').replace(/%}/g, '</u>');
+		    .replace(/<span class="cm-operator">&lt;\*<\/span>/g, '<u>')
+		    .replace(/<span class="cm-operator">\*&gt;<\/span>/g, '</u>')
+		    .replace(/{%/g, '<u>').replace(/%}/g, '</u>');
 	});
 
-}(window.aloha, window.CodeMirror));
+	var $window = $(window);
+
+	var state = {
+		offsets  : [],
+		viewport : {width: 0, height: 0}
+	};
+
+	function onresize() {
+		state.offsets = [];
+		state.viewport = {
+			width  : $window.width(),
+			height : $window.height()
+		};
+		$('header').each(function (i, elem) {
+			var $elem = $(elem);
+			var offset = $elem.offset().top;
+			state.offsets.push([offset, offset + $elem.height(), $elem.find('.bg')]);
+		});
+	}
+
+	var VENDOR_PREFIX = (function () {
+		var elem = document.createElement('div');
+		var prefixes = ['', '-webkit-', '-moz-', '-ms-', '-o-'];
+		var style = elem.style;
+		for (var i = 0; i < prefixes.length; i++) {
+			if (style.hasOwnProperty(prefixes[i] + 'transform')) {
+				return prefixes[i];
+			}
+		}
+		return '';
+	}());
+
+	$(function () {
+		if (0 === $('header .bg').length) {
+			return;
+		}
+		window.requestAnimationFrame(function parallax() {
+			var yStart = $window.scrollTop();
+			var yEnd = yStart + state.viewport.height;
+			state.offsets.forEach(function (offsets) {
+				if (offsets[0] < yEnd && offsets[1] > yStart) {
+					var position = Math.round(yStart - offsets[0]) / 2;
+					offsets[2].css(
+						VENDOR_PREFIX + 'transform',
+						'translate3d(0, ' + position + 'px, 0)'
+					);
+				}
+			});
+			window.requestAnimationFrame(parallax);
+		});
+	});
+
+	function delayed(fn, delay) {
+		var timeout = null;
+		return function () {
+			if (timeout) {
+				clearTimeout(timeout);
+			}
+			timeout = setTimeout(fn, delay);
+		};
+	}
+
+	$window.on('resize', delayed(onresize, 50));
+
+	onresize();
+
+}(window.aloha, window.jQuery, window.CodeMirror));
